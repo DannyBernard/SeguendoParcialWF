4715795a00beb10243fc26f5d26a2ac9e25928f5
DANNY BERNARD <danny.bernard@hotmail.com>
2019-10-30 20:31:56 -0400

    Test

--- BLLTests/RepositorioBaseTests.cs
+++ BLLTests/RepositorioBaseTests.cs
@@ -23,6 +23,15 @@ namespace BLL.Tests
 			RepositorioBase<Clientes> repositorio = new RepositorioBase<Clientes>();
 
 			Assert.IsTrue(repositorio.Guardar(cliente));
+
+			Transacciones transacciones = new Transacciones();
+			transacciones.Fecha = DateTime.Now;
+			transacciones.Tipo = "Deuda";
+			transacciones.Monto = 2000;
+
+			RepositorioBase<Transacciones> repositori = new RepositorioBase<Transacciones>();
+
+			Assert.IsTrue(repositori.Guardar(transacciones));
 		}
 
 		[TestMethod()]
@@ -35,6 +44,16 @@ namespace BLL.Tests
 
 			RepositorioBase<Clientes> repositorioBase = new RepositorioBase<Clientes>();
 			Assert.IsTrue(repositorioBase.Modificar(clientes));
+
+			Transacciones transacciones = new Transacciones();
+
+			transacciones.TransanccionesID = 1;
+			transacciones.Fecha = DateTime.Now;
+			transacciones.Tipo = "Tarjeta";
+			transacciones.Monto = 400;
+
+			RepositorioBase<Transacciones> repositorioBase1 = new RepositorioBase<Transacciones>();
+			Assert.IsTrue(repositorioBase1.Modificar(transacciones));
 		}
 
 		[TestMethod()]
@@ -44,6 +63,11 @@ namespace BLL.Tests
 			RepositorioBase<Clientes> repositorioBase = new RepositorioBase<Clientes>();
 			clientes.ClienteID = 1;
 			Assert.AreEqual(true, repositorioBase.Eliminar(clientes.ClienteID));
+
+			Transacciones transacciones = new Transacciones();
+			RepositorioBase<Transacciones> repositorioBase1 = new RepositorioBase<Transacciones>();
+			transacciones.TransanccionesID = 1;
+			Assert.AreEqual(true, repositorioBase1.Eliminar(transacciones.TransanccionesID));
 		}
 	}
 }
